f8de67df3539e6fce1c19602f0ccd24620ba11b0
Adachi Masakazu <marscode48@gmail.com>
2022-03-04 17:44:59 +0900

    jqueryでスムーススクロールを作成

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -20,4 +20,24 @@ $(function() {
   $('.header__navi a').on('click', function() {
     $('.header').removeClass('open');
   })
+
+  /*=================================================
+  スムーススクロール
+  ===================================================*/
+
+  // ＃から始まるリンクを取得
+  $('a[href^="#"]').click(function() {
+    console.log(this);
+    // リンクをクリックした時のジャンプ先のid名を取得
+    let href= $(this).attr("href");
+    console.log(href)
+    // 空白or#のみだったらリンク先を「html」に設定
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    // トップからジャンプ先の要素までの距離を取得
+    let position = target.offset().top;
+    console.log(position)
+    // スムーズスクロールを実行
+    $("html, body").animate({scrollTop:position}, 600, "swing");
+    return false;
+  });
 });
