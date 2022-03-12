6de62861a6fffed8f866b338a79062beb05aeaba
Adachi Masakazu <marscode48@gmail.com>
2022-03-12 13:37:47 +0900

    JavaScriptでスクロール時の画像フェードを作成

--- scripts/main.js
+++ scripts/main.js
@@ -9,11 +9,24 @@ document.addEventListener('DOMContentLoaded', function () {
 class Main {
   constructor() {
     this._init();
+    this._scrollInit();
   }
 
   _init() {
     new MenuOpen();
     new SmoothScroll(80);
     new HeroSlider('.swiper');
+  }
+
+  _inviewAnimation(el, inview) {
+    console.log(el)
+    console.log(inview)
+    if(inview) {
+      el.classList.add('inview');
+    }
+  }
+
+  _scrollInit() {
+    new ScrollObserver('.feature__img', this._inviewAnimation);
   }
 }
