39302193b0bc0c3784583b37c7fb12a3872f691f
Adachi Masakazu <marscode48@gmail.com>
2022-03-12 14:42:46 +0900

    scroll-polyfillを読み込む

--- scripts/libs/scroll.js
+++ scripts/libs/scroll.js
@@ -32,6 +32,9 @@ class ScrollObserver {
 
     this.io = new IntersectionObserver(callback.bind(this), this.options);
 
+    // @see https://github.com/w3c/IntersectionObserver/tree/master/polyfill
+    this.io.POLL_INTERVAL = 100;
+
     this.els.forEach(el => this.io.observe(el));
   }
 }
